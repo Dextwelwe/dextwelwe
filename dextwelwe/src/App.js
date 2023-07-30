2eb9b0017b4d33cdee5fda4b0352e5c3ca041899
Danil <dextwelwe@gmail.com>
2023-07-30 17:02:42 -0400

    main page view

---
+++ dextwelwe/src/App.js
@@ -0,0 +1,15 @@
+import Mainpage from './main_page/mainpage';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+    <div className='bg'>
+    <Mainpage> </Mainpage>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ dextwelwe/src/main_page/mainpage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Mainpage() {
+  return (
+    <div className=' d-flex justify-content-center'>
+      <div id='content'>
+      <div className='logo'>Dextwelwe</div>
+      <div className='menuMainPage'>
+      <button  className='btnView'> About me</button>
+      <button className='btnView'> Projects</button>
+      <button className='btnView'> Photography</button>
+      <button className='btnView '> Contact me</button>
+      </div>
+      </div>
+    </div>
+  )
+}
