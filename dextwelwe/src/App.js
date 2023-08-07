64161b7438cfbceb4a48bda058fdddce1cb779e5
Danil <dextwelwe@gmail.com>
2023-08-07 14:14:49 -0400

    front page view done

--- dextwelwe/src/App.js
+++ dextwelwe/src/App.js
@@ -1,11 +1,16 @@
 import Mainpage from './main_page/mainpage';
 import './App.css';
+import MetaTags from 'react-meta-tags';
+
 
 function App() {
-
   return (
     <>
     <div className='bg'>
+    <MetaTags>
+    <title>Dextwelwe</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </MetaTags>
     <Mainpage> </Mainpage>
     </div>
     </>
