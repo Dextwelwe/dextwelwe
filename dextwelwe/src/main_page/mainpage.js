e7d1b70f9aa6076ec86df7d4248b1cb65d955005
Danil <dextwelwe@gmail.com>
2023-08-29 22:18:49 -0400

    contact me page done

--- dextwelwe/src/main_page/mainpage.js
+++ dextwelwe/src/main_page/mainpage.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import {useState} from 'react';
 import About from '.././menu/about.js'
-import { useState} from 'react';
 import Projects from '../menu/projects.js';
+import Contactme from '../menu/contactme.js';
 
 export default function Mainpage() {
   const [abt, setAbt] = useState('none');
   const [projects, setProjects] = useState('none');
+  const [contact, setContact] = useState('none');
 
   function setAbout(){
     if (abt === 'none'){
@@ -23,17 +25,27 @@ export default function Mainpage() {
     }
   }
 
+  function setContactMe(){
+    if(contact === 'none'){
+      setContact('block');
+    } else {
+      setContact('none');
+    }
+  }
+
   return (
     <div className='d-flex justify-content-center'>
     {abt === 'block' && <About  abt={abt} set={setAbout}></About>}
+    {contact === 'block' && <Contactme contact={contact} set={setContactMe}></Contactme>}
     {projects === 'block' && <Projects projects={projects} set={setProject}></Projects>}
+    
       <div id='content' className='bgSmScr'>
       <h4 className='logo prevent-select'> Dextwelwe</h4>
       <div className='menuMainPage prevent-select'>
       <button  className='btnView' onClick={setAbout}> About me</button>
       <button className='btnView' onClick={setProject}> Projects </button>
-      <button className='btnView'>Photography</button>
-      <button className='btnView '>Contact me </button>
+      <button className='btnView' onClick={()=>alert('Coming soon')}> Photography</button>
+      <button className='btnView' onClick={setContactMe}>Contact me</button>
       </div>
       </div>
       </div>
---
+++ dextwelwe/src/menu/contactme.js
@@ -0,0 +1,47 @@
+import React , {useRef} from 'react'
+import '../menu/contactme.css'
+import '../App.css'
+import './about.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import ig from '../icons/instagram.png'
+import gh from '../icons/github.png'
+import gm from '../icons/gmail.png'
+import li from '../icons/linkedin.png'
+
+export default function Contactme({contact, set}) {
+    const modal = useRef('');
+    const text = useRef('');
+
+        async function closeModal(){
+        modal.current.style.setProperty("animation-name", "disapearContactMe");
+        text.current.style.setProperty("animation-name", "removeText");
+        }
+
+       function close(){
+        console.log('c')
+        closeModal().then(setTimeout(set, 500));
+      }
+
+  return (
+    <>
+    <div>
+       <div  style={{'display' :  contact}} id="myModal" className={"modal fontFamily"} >
+    <div ref={modal}  className={"modal-contentContactMe"}>
+    <div ref={text} className='textContent flex justify-content-between '>
+      <div className='header'>
+      <h2 className='fontColor px-2'>Contact me</h2>
+      <button onClick={close} className={"close mx-2"}>&times;</button> 
+      </div>
+    <div className=' skillBoxContactMe mt-2 d-grid p-3 mx-3'>
+     <a className='textLink' target="_blank"  rel="noopener noreferrer" href='https://github.com/Dextwelwe'><span><img src={gh} alt='github' className='imgLogo'></img> Github</span> </a>
+     <a className='textLink' href='\\'><span><img src={li} alt='linkedin' className='imgLogo'></img> LinkedIn</span></a>
+     <a className='textLink' target="_blank"  rel="noopener noreferrer" href='https://www.instagram.com/dextwelwe/' ><span><img src={ig} alt='instagram' className='imgLogo'></img> Instagram</span></a>
+     <a className='textLink' target="_blank"  rel="noopener noreferrer" href='https://mail.google.com/mail/u/?view=cm&to=dextwelwe@gmail.com'><span><img src={gm} alt='gmail' className='imgLogo'></img> dextwelwe@gmail.com</span></a>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </>
+  )
+}
